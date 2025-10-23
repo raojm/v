4c34eae778be0d4d9adcb9353ef305db02ef04de
raojm <raojm@91ne.com>
2025-10-23 14:50:38 +0800

    static libs do not need to have a main

--- vlib/v/checker/checker.v
+++ vlib/v/checker/checker.v
@@ -459,6 +459,10 @@ pub fn (mut c Checker) check_files(ast_files []&ast.File) {
 		// shared libs do not need to have a main
 		return
 	}
+	if c.pref.is_staticlib {
+		// static libs do not need to have a main
+		return
+	}
 	if c.pref.no_builtin {
 		// `v -no-builtin module/` do not necessarily need to have a `main` function
 		// This is useful for compiling linux kernel modules for example.
